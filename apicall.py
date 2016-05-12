9b2b95e5eb4ad742e67a415fc66680c9378bbc28
Ryan Smith <smithrk81@gmail.com>
2016-05-11 17:38:03 -0700

    Added logic to check for entries

--- apicall.py
+++ apicall.py
@@ -8,10 +8,25 @@ import json
 import export 
 
 fileName = raw_input("Name your output File: ")
+if fileName == "":
+	print("you didn't enter a file name")
+	break
 apiKey = raw_input("\nEnter the Access Token: ")
+if apiKey == "":
+	print("you didn't enter an API Key")
+	break
 pubKey = raw_input("\nEnter the Publisher Key: ")
+if pubKey == "":
+	print("you didn't enter a publisher key")
+	break
 startDate = raw_input("\nEnter the start date: (YYYY-MM-DD)")
+if startDate == "":
+	print("you didn't enter a date")
+	break
 endDate = raw_input("\nEnter the end date: (YYYY-MM-DD)")
+if endDate == "":
+	print("you didn't enter a date")
+	break
 
 
 # Replace with the correct URL
@@ -25,7 +40,7 @@ myResponse = requests.get(urlDaily, verify=True)
 
 dailyJSON = json.loads(myResponse.content)
 
-export.exportDailyStats(dailyJSON['rows'],'firstTest')
+export.exportDailyStats(dailyJSON['rows'],fileName)
 
 myResponse = requests.get(urlServices, verify=True)
 
