a596ac892033dcc587332a2180cdb13bfe538e4c
Ryan Smith <smithrk81@gmail.com>
2016-05-12 09:37:50 -0700

    added call for services and export processing

--- apicall.py
+++ apicall.py
@@ -6,9 +6,11 @@ import requests
 from requests.auth import HTTPDigestAuth
 import json
 import export 
+import helpers
+
 
 fileName = ""
-apiKey = ""
+accessToken = ""
 pubKey = ""
 startDate = ""
 endDate = ""
@@ -17,37 +19,47 @@ while fileName == "":
 	fileName = raw_input("File name: ")	
 	if fileName == "Exit" or fileName == "exit":
 		sys.exit("Stopping")
-while apiKey == "":
-	apiKey = raw_input("\nEnter the Access Token: ")
-	if apiKey == "Exit" or fileName == "exit":
+while accessToken == "":
+	accessToken = raw_input("\nEnter the Access Token: ")
+	if accessToken == "Exit" or accessToken == "exit":
 		sys.exit("Stopping")
 while pubKey == "":
 	pubKey = raw_input("\nEnter the Publisher Key: ")
-	if pubKey == "Exit" or fileName == "exit":
+	if pubKey == "Exit" or pubKey == "exit":
 		sys.exit("Stopping")
-while startDate == "":
+while startDate == "" or len(startDate) < 10 or len(startDate) > 10:
 	startDate = raw_input("\nEnter the start date: (YYYY-MM-DD)")
-	if startDate == "Exit" or fileName == "exit":
+	if startDate == "Exit" or startDate == "exit":
 		sys.exit("Stopping")
-while endDate == "":
+	if not helpers.dateCheck(startDate):
+		print("You entered an invalid date format")
+
+while endDate == "" or len(startDate) < 10 or len(startDate) > 10:
 	endDate = raw_input("\nEnter the end date: (YYYY-MM-DD)")
-	if endDate == "Exit" or fileName == "exit":
+	if endDate == "Exit" or endDate == "exit":
 		sys.exit("Stopping")
-	
+	if not helpers.dateCheck(startDate):
+		print("You entered an invalid date format")
 
 
 # Replace with the correct URL
 urlDaily = "http://api.po.st/analytics/v1/daily.json?access-token="+accessToken+"&pubkey="+pubKey+"&period-from="+startDate+"&period-to="+endDate
 urlServices = "http://api.po.st/analytics/v1/services.json?access-token="+accessToken+"&pubkey="+pubKey+"&period-from="+startDate+"&period-to="+endDate
 # It is a good practice not to hardcode the credentials. So ask the user to enter credentials at runtime
-myResponse = requests.get(urlDaily, verify=True)
-#print (myResponse.status_code)
+myResponseDaily = requests.get(urlDaily, verify=True)
 
+myResponseServices = requests.get(urlServices, verify=True)
 # For successful API call, response code will be 200 (OK)
 
-dailyJSON = json.loads(myResponse.content)
+dailyJSON = json.loads(myResponseDaily.content)
 
-export.exportDailyStats(dailyJSON['rows'],fileName)
 
-myResponse = requests.get(urlServices, verify=True)
+
+servicesJSON = json.loads(myResponseServices.content)
+
+print(servicesJSON['rows'])
+export.exportDailyStats(dailyJSON['rows'], servicesJSON['rows'],fileName)
+
+
+
 
--- export.py
+++ export.py
@@ -1,16 +1,17 @@
 import xlsxwriter
 
-def exportDailyStats(apiResp,name):
-    print('inside the function')
+def exportDailyStats(apiDaily, apiServices,name):
     # Create an new Excel file and add a worksheet.
     workbook = xlsxwriter.Workbook(name+'.xlsx')
     worksheet = workbook.add_worksheet()
 
     # Widen the first column to make the text clearer.
     worksheet.set_column('A:A', 20)
-    worksheet.set_column('B:B', 10)
-    worksheet.set_column('C:C', 10)
-
+    worksheet.set_column('B:B', 5)
+    worksheet.set_column('C:C', 5)
+    worksheet.set_column('F:F', 20)
+    worksheet.set_column('G:G', 5)
+    worksheet.set_column('H:H', 5)
     # Add a bold format to use to highlight cells.
     bold = workbook.add_format({'bold': True})
 
@@ -18,10 +19,13 @@ def exportDailyStats(apiResp,name):
     worksheet.write('A1', 'Date', bold)
     worksheet.write('B1', 'Clicks', bold)
     worksheet.write('C1', 'Shares',bold)
+    worksheet.write('F1', 'Service', bold)
+    worksheet.write('G1', 'Clicks', bold)
+    worksheet.write('H1', 'Shares', bold)
 
     rowNum = 2
 
-    for item in apiResp:
+    for item in apiDaily:
         for sub in item:
             if sub == 'date':
                 worksheet.write('A'+str(rowNum), item[sub])
@@ -30,4 +34,18 @@ def exportDailyStats(apiResp,name):
             else:
                 worksheet.write('C'+str(rowNum), item[sub])
         rowNum += 1
+    
+    rowNum = 2
+
+    for item in apiServices:
+        for sub in item:
+            if sub == 'service':
+                worksheet.write('F'+str(rowNum), item[sub])
+            elif sub == 'clicks':
+                worksheet.write('G'+str(rowNum), item[sub])
+            elif sub == 'shares':
+                worksheet.write('H'+str(rowNum), item[sub])
+        rowNum += 1
     workbook.close()
+
+
