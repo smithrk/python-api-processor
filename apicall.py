f0f2dcc1c8545fa159882c61c3484fc047881e7d
Ryan Smith <smithrk81@gmail.com>
2016-05-11 17:52:32 -0700

    added exit option to entries

--- apicall.py
+++ apicall.py
@@ -14,15 +14,25 @@ startDate = ""
 endDate = ""
 
 while fileName == "":
-	fileName = raw_input("Name your output File: ")	
+	fileName = raw_input("File name: ")	
+	if fileName == "Exit" or fileName == "exit":
+		break
 while apiKey == "":
 	apiKey = raw_input("\nEnter the Access Token: ")
+	if apiKey == "Exit" or fileName == "exit":
+		break
 while pubKey == "":
 	pubKey = raw_input("\nEnter the Publisher Key: ")
+	if pubKey == "Exit" or fileName == "exit":
+		break
 while startDate == "":
 	startDate = raw_input("\nEnter the start date: (YYYY-MM-DD)")
+	if startDate == "Exit" or fileName == "exit":
+		break
 while endDate == "":
 	endDate = raw_input("\nEnter the end date: (YYYY-MM-DD)")
+	if endDate == "Exit" or fileName == "exit":
+		break
 	
 
 
